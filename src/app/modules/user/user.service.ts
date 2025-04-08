37d9fd129dfd3e281c15d3e4874697070b194add
gazi275 <gaziazad270@gmail.com>
2025-04-09 01:51:34 +0600

    bugs

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,11 +2,7 @@ import { IUser } from "./user.interface";
 import { User } from "./user.model";
 
 const createUser =async (user : IUser)=>{
-    const user = await User.findOne({ email: user.email });
-    if (user) {
-
-        throw new Error('User already exists');
-    }
+   
     const newUser = await User.create(user)
     return newUser;
 }
